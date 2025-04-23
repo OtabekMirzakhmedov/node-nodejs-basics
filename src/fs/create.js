9c1692008aa7e00db8046661314e1230536f7651
OtabekMirzakhmedov <otabek.mirzakhmedov@gmail.com>
2025-04-23 16:46:08 +0500

    create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 const create = async () => {
-    // Write your code here 
+    const content = 'I am fresh and young';
+    const filePath = path.join('files', 'fresh.txt');
+    const fileExists = await fs.access(filePath)
+        .then(() => true)
+        .catch(() => false);
+    if (fileExists) {
+        throw new Error('FS operation failed');
+    }
+    await fs.writeFile(filePath, content, (err) => {
+        if (err) throw err;
+    });
 };
 
 await create();
