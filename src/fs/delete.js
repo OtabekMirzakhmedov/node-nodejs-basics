b83833089768ed513ba59281567c8f7469491329
OtabekMirzakhmedov <otabek.mirzakhmedov@gmail.com>
2025-04-24 16:31:12 +0500

    files and environments

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,11 @@
+import fs from 'fs/promises'
+
 const remove = async () => {
-    // Write your code here 
+    const fileExist = await fs.access('./files/fileToRemove.txt').then(() => true).catch(() => false);
+    if (!fileExist) {
+        throw new Error('FS operation failed');
+    }
+    await fs.unlink('./files/fileToRemove.txt');
 };
 
 await remove();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,12 @@
+import fs from 'fs/promises';
+
 const rename = async () => {
-    // Write your code here 
+
+    const fileExist = await fs.access('./files/wrongFilename.txt').then(() => true).catch(() => false);
+    if (!fileExist) {
+        throw new Error('FS operation failed');
+    }
+    await fs.rename('./files/wrongFilename.txt', './files/properFilename.md');
 };
 
 await rename();
