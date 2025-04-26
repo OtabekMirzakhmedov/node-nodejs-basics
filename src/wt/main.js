8a22f060d378ca836ae1292aa3fad9793d0a2a9e
OtabekMirzakhmedov <otabek.mirzakhmedov@gmail.com>
2025-04-26 20:19:09 +0500

    worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,26 @@
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
+    const numCores = cpus().length;
+    console.log(`Number of CPU cores: ${numCores}`);
+    const workerPath = './worker.js';
+    const workerPromises = [];
+    for (let i = 0; i < numCores; i++) {
+        const workerData = 10 + i;
+        const worker = new Worker(workerPath, {workerData});
+        workerPromises.push(new Promise((resolve, reject) => {
+            worker.on('message', (result) => {
+                resolve({ status: 'resolved', data: result });
+            });
+            worker.on('error', (error) => {
+                reject({ status: 'error', data: null });
+            });
+        }));
+    }
+    const workerResults = await Promise.allSettled(workerPromises);
+    console.log(workerResults);
+    return workerResults;
 };
 
 await performCalculations();
